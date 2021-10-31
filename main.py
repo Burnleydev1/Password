4c5d93cc9409932dced339773da583f3422d893d
Burnleydev1 <abongwabonalais@gmail.com>
2021-10-31 17:48:22 +0100

    characters added

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import string
 import random
+
+# characters to generate password from
+alphabets = list(string.ascii_letters)
+digits = list(string.digits)
+special_characters = list("!@#$%^&*()")
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
